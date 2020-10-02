8885bce7ca59e377b7a648007152727beb1f41ab
root <sam2001rufus@gmail.com>
2020-10-02 17:36:34 +0000

    port declaration

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.use(indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
-app.listen( 3000 ,function(){
-	console.log("Yelp Camp has Started y5!!");
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("YelpCamp has started!!!");
 });
